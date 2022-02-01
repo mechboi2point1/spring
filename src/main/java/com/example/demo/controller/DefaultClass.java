1f0ed17e819e0181819de0cedbd0a95db2db86a6
Animesh Samanta <77837209+mechboi2point1@users.noreply.github.com>
2022-02-01 18:56:20 +0530

    Connec t

---
+++ src/main/java/com/example/demo/controller/DefaultClass.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class DefaultClass {
+@GetMapping(value="")
+public String getNull() {
+	return "Connect to DEV team at meet@theanimesh.in for API Docs";
+}
+}
